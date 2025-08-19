1f2665d67261d14734212d366920a9d500c285e7
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-08-19 18:43:24 +0800

    feat: make sure the ownership transfering properly in share_ptr construction.

--- header/ASTAnnotation/Symbol.hpp
+++ header/ASTAnnotation/Symbol.hpp
@@ -15,7 +15,7 @@ private:
   std::shared_ptr<TypeKind> type_;
 
 public:
-  SymbolVariableInfo(const std::string &name, std::shared_ptr<TypeKind> type);
+  SymbolVariableInfo(const std::string &name, std::shared_ptr<TypeKind>&& type);
   ~SymbolVariableInfo();
   auto getName() const -> const std::string &;
   auto getType() const -> std::shared_ptr<TypeKind>;
@@ -29,8 +29,8 @@ private:
 
 public:
   SymbolFunctionInfo(const std::string &name,
-                     std::shared_ptr<TypeKind> returnType,
-                     std::vector<std::shared_ptr<TypeKind>> parameters);
+                     std::shared_ptr<TypeKind>&& returnType,
+                     std::vector<std::shared_ptr<TypeKind>>&& parameters);
   ~SymbolFunctionInfo();
   auto getName() const -> const std::string &;
   auto getReturnType() const -> std::shared_ptr<TypeKind>;
@@ -44,7 +44,7 @@ private:
   std::shared_ptr<TypeDef> type_;
 
 public:
-  SymbolTypeInfo(const std::string &name, std::shared_ptr<TypeDef> type);
+  SymbolTypeInfo(const std::string &name, std::shared_ptr<TypeDef>&& type);
   ~SymbolTypeInfo();
   auto getName() const -> const std::string &;
   auto getType() const -> std::shared_ptr<TypeDef>;
