77979622c71852691a775c727a8fbb978cc65824
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-08-25 18:13:31 +0800

    .ci: add test for official testpoints.

---
+++ .ci/semantic_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+echo "--- Configuring CMake ---"
+cmake -B build
+
+echo "--- Building project ---"
+cmake --build build --config Release
+
+echo "--- Running semantic tests ---"
+./build/tests/run_tests --gtest_filter=AllTest.*
