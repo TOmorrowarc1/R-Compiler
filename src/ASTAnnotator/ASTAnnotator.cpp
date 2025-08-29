511f6a2f4b04a04449b955b39e377c272c73d9f2
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-08-29 15:40:34 +0800

    feat: reflect the CMAKE and the annotator.

--- src/ASTAnnotator/ASTAnnotator.cpp
+++ src/ASTAnnotator/ASTAnnotator.cpp
@@ -1,22 +1,26 @@
 #include "ASTAnnotator.hpp"
 #include "ASTRootNode.hpp"
+#include "ConstEvaluator.hpp"
+#include "ConstTypeCollector.hpp"
+#include "ConstTypeEvaluator.hpp"
+#include "FunctionCollector.hpp"
 #include "Scope.hpp"
 #include "ScopeBuiltInInit.hpp"
 #include "SemanticChecker.hpp"
-#include "SymbolAnnotator.hpp"
-#include "SymbolCollector.hpp"
-#include "TypeKind.hpp"
-#include "ValueInfo.hpp"
 
 void ASTAnnotate(ASTRootNode *root, Scope *initial_scope) {
+  ConstEvaluator const_evaluator;
   scopeBuildInInit(initial_scope);
-  SymbolCollector collector(initial_scope);
+  ConstTypeCollector collector(initial_scope, &const_evaluator);
   root->accept(collector);
   initial_scope->resetIndex();
-  SymbolAnnotator annotator(initial_scope);
-  root->accept(annotator);
+  ConstTypeEvaluator evaluator(initial_scope, &const_evaluator);
+  root->accept(evaluator);
   initial_scope->resetIndex();
-  SemanticChecker checker(initial_scope);
+  FunctionCollector function_collector(initial_scope, &const_evaluator);
+  root->accept(function_collector);
+  initial_scope->resetIndex();
+  SemanticChecker checker(initial_scope, &const_evaluator);
   root->accept(checker);
   initial_scope->resetIndex();
 }
